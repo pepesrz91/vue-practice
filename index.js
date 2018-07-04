d2df9eaa2c3116de50bef9bb27bd89835df50740
DESKTOP-2UG043F\pepe_ <pepesrzra@outlook.com>
2018-07-04 16:06:07 -0500

    Starting using the weather app with maps.googleapis.com

---
+++ index.js
@@ -0,0 +1,19 @@
+//var Vue = require('vue');
+//import Vue from 'vue';
+
+var helloWorld = new Vue({
+  el:'#helloVue',
+  data:{
+    title:"Hello World!!!"
+  }
+});
+
+//console.log(helloWorld);
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    name: "Hello World",
+    message: 'Hello Pepe!'
+  }
+});
